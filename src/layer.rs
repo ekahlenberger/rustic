0db0a512632fa6075e2b9bcb387d8611ee5b458d
Eberhard Kahlenberger <github@e.kahlenberger.net>
2023-04-30 22:39:28 +0200

    added L1 and L2 again

--- src/layer.rs
+++ src/layer.rs
@@ -65,19 +65,19 @@ impl Layer
             if clipped_delta.abs() < 1e-6 {
                 continue;
             }
-            let learn_delta = clipped_delta * learning_rate;
+            
             for (input_node_index, input_value) in input.iter().enumerate() {
                 let w = &mut self.weights[input_node_index][output_node_index];
-                // let l1_regularization = Self::L1_REGULARIZATION * w.signum();
-                // let l2_regularization = Self::L2_REGULARIZATION * *w;
+                let l1_regularization = Self::L1_REGULARIZATION * w.signum();
+                let l2_regularization = Self::L2_REGULARIZATION * *w;
     
-                *w += learning_rate * (clipped_delta * input_value /* - (l1_regularization + l2_regularization)*/);
+                *w += learning_rate * (clipped_delta * input_value - (l1_regularization + l2_regularization));
             }
-
+            let learn_delta = clipped_delta * learning_rate;
             let bias = self.biases[output_node_index];
-            // let l1_regularization = Self::L1_REGULARIZATION * bias.signum();
-            // let l2_regularization = Self::L2_REGULARIZATION * bias;
-            self.biases[output_node_index] += learn_delta /* - learning_rate * (l1_regularization + l2_regularization) */;
+            let l1_regularization = Self::L1_REGULARIZATION * bias.signum();
+            let l2_regularization = Self::L2_REGULARIZATION * bias;
+            self.biases[output_node_index] += learn_delta - learning_rate * (l1_regularization + l2_regularization);
         }
     }
 }
